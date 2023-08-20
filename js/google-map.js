86b8e5efc289e48fc3098b3cd4c185fb4e88425e
MushifJutt <120536695+MushifJutt@users.noreply.github.com>
2023-08-20 19:39:54 +0500

    MyLovelyFamilywebsite

---
+++ js/google-map.js
@@ -0,0 +1,62 @@
+
+var google;
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+    var myLatlng = new google.maps.LatLng(30.9272107, 72.7290143);
+    // 39.399872
+    // -8.224454
+    
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 7,
+
+        // The latitude and longitude to center the map (always required)
+        center: myLatlng,
+
+        // How you would like to style the map. 
+        scrollwheel: false,
+        styles: [
+            {
+                "featureType": "administrative.country",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    },
+                    {
+                        "hue": "#ff0000"
+                    }
+                ]
+            }
+        ]
+    };
+
+    
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using out element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+    
+    var addresses = ['Pakistan'];
+
+    for (var x = 0; x < addresses.length; x++) {
+        $.getJSON('https://www.google.com/maps/place/Chak+198+GB,+Faisalabad,+Punjab,+Pakistan/@30.9272107,72.7290143,16z/data=!3m1!4b1!4m6!3m5!1s0x3922e23fd6317e2f:0xb1a5a92daac5e9d9!8m2!3d30.925188!4d72.7348381!16s%2Fg%2F1tppqbwp?entry=ttu'+addresses[x]+'&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location
+            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            new google.maps.Marker({
+                position: latlng,
+                map: map,
+                icon: 'images/loc.png'
+            });
+
+        });
+    }
+    
+}
+google.maps.event.addDomListener(window, 'load', init);
